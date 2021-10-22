777d55f5b301b4696c9397b4a44f3523b26ffde5
unknown <jgarciap@ext.laliga.es>
2021-10-22 11:21:02 +0200

    chore: wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Extension, LCDClient, MnemonicKey, Wallet } from '@terra-money/terra.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'terra-angular';
+  wallet!: Wallet;
+  requestInfo: any;
+
+  async connect() {
+    const extension = new Extension();
+    extension.connect();
+    extension.on("connect", (w: Wallet) => {
+      w = this.wallet
+      console.log('***' + w);
+    });
+
+    console.log(extension.isAvailable);
+    extension.request('connect').then(data => {
+      this.requestInfo = data.payload;
+      console.log(data.payload);
+    });
+
+  }
 }
